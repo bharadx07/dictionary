ea3f521a33bcc06b739a8cb7141e83085fd978d5
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-11-26 19:23:42 -0800

    init

---
+++ CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json; //Modules with nuget
+
+
+namespace Csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Chipher chipher = new Chipher();
+
+            string encrypted = chipher.encrypt("Prakash");
+
+            Console.WriteLine(encrypted);
+
+            string decrypted = chipher.decrypt(encrypted);
+
+            Console.WriteLine(decrypted);
+
+            var value = JsonConvert.DeserializeObject("{}");
+        }
+    }
+
+    //Classes
+    class Chipher
+    {
+        const string INSERTION_KEY = "dfskjljjjljljljljljljljljljljljljljljljljljljljljlj9er92038fidsusmfkk3^67fjakkleulutiosdfnlasf"; //Combo of js var defs (const, let, var) and java type defs
+
+        public string encrypt(string plaintext)
+        {
+            return string.Join("", plaintext.Select(c => c + INSERTION_KEY));
+        }
+
+        public string decrypt(string Chipher)
+        {
+            string decrypted = "";
+            for (int i = 0; i < Chipher.Length; i += INSERTION_KEY.Length + 1)
+            {
+                decrypted += Chipher[i];
+            }
+
+            return decrypted;
+
+
+        }
+    }
+}
+
