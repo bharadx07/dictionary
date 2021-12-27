687069da16eb7231e1bfa93e2c2e02864b975d04
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-12-26 21:29:49 -0800

    some new tech

--- tech/python-childprocess/index.js
+++ tech/python-childprocess/index.js
@@ -1,3 +1,10 @@
-const spawn = require("child_process").spawn
-const instance = spawn("python", ['process.py', 1, 2])
-instance.stdout.on("data", data => console.log(data.toString()))
+// library to exc the python
+const spawn = require("child_process").spawn;
+
+// create our executor. pass file path and inputs
+const exc = spawn("python", ["process.py", 10, 20]);
+
+// callback: when we get data, print the it. data is raw so we convert to string
+exc.stdout.on("data", raw => {
+    console.log(raw.toString());
+})
